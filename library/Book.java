84cad237f8f9d48ed751e36e8a381f019c97318b
gdangga <gedeangga424@gmail.com>
2023-05-14 15:08:24 +0800

    tes coba

--- library/Book.java
+++ library/Book.java
@@ -1,4 +1,16 @@
 class Book {
   public String id;
   public String title;
+  
+  public void setId(String bookId) {
+    this.id = bookId;
+  }
+  
+  public void setId(int bookId) {
+    this.id = Integer.toString(bookId);
+  }
+  
+  public Object getId() {
+    return null;
+  }
 }
